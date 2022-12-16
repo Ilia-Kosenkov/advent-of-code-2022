ea775d57d8e396e45ff0b37e3a810ec2c8a67f15
Ilia <ilia.kosenkov.at.gm@gmail.com>
2022-12-16 20:42:44 +0200

    Task 02 (#3)
    * Add files via upload
    
    * Rename main.rs to 02/src/main.rs
    
    * Rename Cargo.lock to 02/Cargo.lock
    
    * Rename Cargo.toml to 02/Cargo.toml
    
    * Rename .gitignore to 02/.gitignore
    
    * Create .gitingore
    
    * Fix gitignore
    
    * Finished part 1
    
    * Solved part 2
    
    * Do things mathy way
    
    * Remove input
    
    * Update gitignore

---
+++ 02/src/main.rs
@@ -0,0 +1,164 @@
+use std::{
+    io::{self, BufRead},
+    str::FromStr,
+};
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum RoundResult {
+    Win = 6,
+    Draw = 3,
+    Loss = 0,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Hand {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct MyHand(Hand);
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct OpponentHand(Hand);
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct KnownRound {
+    opponent_hand: OpponentHand,
+    my_hand: MyHand,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct UnknownRound {
+    opponent_hand: OpponentHand,
+    round_result: RoundResult,
+}
+
+impl KnownRound {
+    fn play(&self) -> RoundResult {
+        use RoundResult::*;
+
+        match self.my_hand.0 as i32 - self.opponent_hand.0 as i32 {
+            0 => Draw,
+            1 | -2 => Win,
+            _ => Loss
+        }
+    }
+}
+
+impl UnknownRound {
+    fn get_best_hand_score(&self) -> i32 {
+        use RoundResult::*;
+
+        match self.round_result {
+            Win => (self.opponent_hand.0 as i32) % 3i32 + 1i32,
+            Loss => (self.opponent_hand.0 as i32 + 1i32) % 3i32 + 1i32,
+            Draw => self.opponent_hand.0 as i32,
+        }
+    }
+}
+
+impl FromStr for OpponentHand {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        s.chars()
+            .next()
+            .and_then(|ch| match ch {
+                'A' => Some(Self(Hand::Rock)),
+                'B' => Some(Self(Hand::Paper)),
+                'C' => Some(Self(Hand::Scissors)),
+                _ => None,
+            })
+            .ok_or(())
+    }
+}
+
+impl FromStr for MyHand {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        s.chars()
+            .next()
+            .and_then(|ch| match ch {
+                'X' => Some(Self(Hand::Rock)),
+                'Y' => Some(Self(Hand::Paper)),
+                'Z' => Some(Self(Hand::Scissors)),
+                _ => None,
+            })
+            .ok_or(())
+    }
+}
+
+impl FromStr for RoundResult {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        s.chars()
+            .next()
+            .and_then(|ch| match ch {
+                'X' => Some(RoundResult::Loss),
+                'Y' => Some(RoundResult::Draw),
+                'Z' => Some(RoundResult::Win),
+                _ => None,
+            })
+            .ok_or(())
+    }
+}
+
+impl FromStr for KnownRound {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        s[..1].parse::<OpponentHand>().and_then(|op_h| {
+            s[2..].parse::<MyHand>().and_then(|my_h| {
+                Ok(KnownRound {
+                    opponent_hand: op_h,
+                    my_hand: my_h,
+                })
+            })
+        })
+    }
+}
+
+impl FromStr for UnknownRound {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        s[..1].parse::<OpponentHand>().and_then(|op_h| {
+            s[2..].parse::<RoundResult>().and_then(|res| {
+                Ok(UnknownRound {
+                    opponent_hand: op_h,
+                    round_result: res,
+                })
+            })
+        })
+    }
+}
+
+fn main() {
+    let reader = io::BufReader::new(io::stdin());
+    let rounds = reader.lines().filter_map(|l| {
+        let l = l.map_err(|_| ());
+        l.and_then(
+            |s| match (s.parse::<KnownRound>(), s.parse::<UnknownRound>()) {
+                (Ok(known), Ok(unknown)) => Ok((known, unknown)),
+                _ => Err(()),
+            },
+        )
+        .ok()
+    });
+
+    let scores = rounds.map(|r| {
+        (
+            r.0.play() as i32 + r.0.my_hand.0 as i32,
+            r.1.get_best_hand_score() as i32 + r.1.round_result as i32,
+        )
+    });
+
+    let total_score = scores.fold((0, 0), |(acc1, acc2), (r1, r2)| (acc1 + r1, acc2 + r2));
+
+    println!("{}", total_score.0);
+    println!("{}", total_score.1);
+}
