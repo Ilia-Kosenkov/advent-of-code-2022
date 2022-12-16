896c529ed880ad29be1ac4597cc8c537061a360c
Ilia <ilia.kosenkov.at.gm@gmail.com>
2022-12-16 17:24:40 +0200

    Update main.rs

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,9 +1,33 @@
 use std::io::{self, BufRead};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+struct RunningMax(i32, i32, i32);
+
+impl RunningMax {
+    fn update(&self, new_value : i32) -> Self {
+        if self.2 < new_value {
+            RunningMax(self.0, self.1, new_value)
+        }
+        else if self.1 < new_value {
+            RunningMax(self.0, new_value, self.2)
+        }
+        else if self.0 < new_value {
+            RunningMax(new_value, self.1, self.2)
+        }
+        else {
+            self.clone()
+        }
+    }
+
+    fn sum(&self) -> i32 {
+        self.0 + self.1 + self.2
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
 struct Accumulator {
     current: i32,
-    running_max: i32,
+    running_max : RunningMax,
 }
 
 fn main() {
@@ -16,13 +40,13 @@ fn main() {
         .fold(
             Accumulator {
                 current: 0i32,
-                running_max: 0i32,
+                running_max: std::default::Default::default(),
             },
             |acc, x| {
                 x.map_or_else(
                     || Accumulator {
                         current: 0i32,
-                        running_max: acc.running_max.max(acc.current),
+                        running_max: acc.running_max.update(acc.current),
                     },
                     |y| Accumulator {
                         current: acc.current + y,
@@ -33,5 +57,6 @@ fn main() {
         )
         .running_max;
 
-    println!("{}", result);
+    println!("{}", result.2);
+    println!("{}", result.sum())
 }
