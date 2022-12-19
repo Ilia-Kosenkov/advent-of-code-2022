59a844fa71a6918e5032c41507c1e123e2f2ba25
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2022-12-20 00:23:02 +0200

    Use sliding window

--- 06/src/main.rs
+++ 06/src/main.rs
@@ -4,10 +4,13 @@ use std::{
 };
 
 use itertools::Itertools;
+use sliding_windows::{IterExt, Storage, Window};
 
 fn unit<T>(_: T) {}
 
 fn main() {
+    let mut storage = Storage::<char>::new(4);
+
     let reader = io::BufReader::new(io::stdin());
     let result = reader
         .lines()
@@ -16,7 +19,7 @@ fn main() {
         .and_then(|x| x.map_err(unit))
         .and_then(|l| {
             l.chars()
-                .tuple_windows::<(_, _, _, _)>()
+                .sliding_windows(&mut storage)
                 .enumerate()
                 .filter_map(|(i, w)| {
                     if are_all_unique(&w) {
@@ -34,7 +37,12 @@ fn main() {
     println!("{:?}", result.unwrap_or(<usize>::MAX));
 }
 
-fn are_all_unique(x: &(char, char, char, char)) -> bool {
+fn are_all_unique(window: &Window<char>) -> bool {
     let mut set = HashSet::<char>::with_capacity(4);
-    set.insert(x.0) && set.insert(x.1) && set.insert(x.2) && set.insert(x.3)
+    for c in window {
+        if !set.insert(*c) {
+            return false;
+        }
+    }
+    return true;
 }
