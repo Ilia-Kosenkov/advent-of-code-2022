6ac48a62a4256f67039835a848ac8e7807d6ab64
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2022-12-24 01:12:02 +0200

    Implement Part 1

---
+++ 09/src/main.rs
@@ -0,0 +1,135 @@
+use std::{
+    collections::HashSet,
+    io::{self, BufRead},
+    str::FromStr,
+};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default, Hash)]
+struct Pos(i32, i32);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Move {
+    direction: Direction,
+    step_count: i32,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default, Hash)]
+struct PosChange(i32, i32);
+
+impl Into<PosChange> for Direction {
+    fn into(self) -> PosChange {
+        match self {
+            Direction::Up => PosChange(0, 1),
+            Direction::Right => PosChange(1, 0),
+            Direction::Down => PosChange(0, -1),
+            Direction::Left => PosChange(-1, 0),
+        }
+    }
+}
+
+impl Pos {
+    fn start() -> Pos {
+        std::default::Default::default()
+    }
+
+    fn move_towards(&mut self, to: &Pos) {
+        let d = self.distance(to);
+        let abs_d = (d.0.abs(), d.1.abs());
+
+        if abs_d.0 + abs_d.1 == 3 {
+            self.0 += d.0.signum();
+            self.1 += d.1.signum();
+        } else if abs_d.0 == 2 {
+            self.0 += d.0.signum();
+        } else if abs_d.1 == 2 {
+            self.1 += d.1.signum();
+        }
+    }
+
+    fn distance(&self, other: &Pos) -> (i32, i32) {
+        (other.0 - self.0, other.1 - self.1)
+    }
+
+    fn r#move(&mut self, dir: &PosChange) {
+        self.0 += dir.0;
+        self.1 += dir.1;
+    }
+}
+
+impl FromStr for Move {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut iter = s.split_whitespace().filter(|ss| !ss.is_empty());
+
+        let direction = iter
+            .next()
+            .ok_or(())
+            .and_then(|c| c.parse::<char>().map_err(unit));
+        let step_count = iter
+            .next()
+            .ok_or(())
+            .and_then(|c| c.parse::<i32>().map_err(unit));
+
+        match (direction, step_count) {
+            (Ok(direction), Ok(step_count)) => match direction {
+                'U' => Ok(Move {
+                    direction: Direction::Up,
+                    step_count: step_count,
+                }),
+                'R' => Ok(Move {
+                    direction: Direction::Right,
+                    step_count: step_count,
+                }),
+                'D' => Ok(Move {
+                    direction: Direction::Down,
+                    step_count: step_count,
+                }),
+                'L' => Ok(Move {
+                    direction: Direction::Left,
+                    step_count: step_count,
+                }),
+                _ => Err(()),
+            },
+            _ => Err(()),
+        }
+    }
+}
+
+fn unit<T>(_: T) {}
+
+fn main() {
+    let reader = io::BufReader::new(io::stdin());
+
+    let input = reader
+        .lines()
+        .map(|l| l.map_err(unit).and_then(|s| s.parse::<Move>()));
+
+    let pos_changes = input.flat_map(|m| match m {
+        Ok(mv) => std::iter::repeat(<Direction as Into<PosChange>>::into(mv.direction))
+            .take(mv.step_count as usize),
+        _ => std::iter::repeat(PosChange::default()).take(0usize),
+    });
+
+    let mut head = Pos::start();
+    let mut tail = Pos::start();
+
+    let mut history = HashSet::<Pos>::new();
+    history.insert(tail);
+
+    for pos_change in pos_changes {
+        head.r#move(&pos_change);
+        tail.move_towards(&head);
+        history.insert(tail);
+    }
+
+    println!("{}", history.len());
+}
