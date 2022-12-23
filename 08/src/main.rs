fec9f2563fefd701773786bb622c7fbe071cdf0e
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2022-12-24 00:05:36 +0200

    Finished Part 2

--- 08/src/main.rs
+++ 08/src/main.rs
@@ -1,7 +1,33 @@
-use std::io::{self, BufRead};
+use std::{
+    fmt::Debug,
+    io::{self, BufRead},
+};
 
 use itertools::Itertools;
 use ndarray::{Array2, Axis};
+
+#[derive(Copy, Clone, Default)]
+struct Visibility {
+    top: usize,
+    right: usize,
+    bottom: usize,
+    left: usize,
+}
+
+impl Debug for Visibility {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!(
+            "({},{},{},{})",
+            &self.top, &self.right, &self.bottom, &self.left
+        ))
+    }
+}
+
+impl Visibility {
+    fn area(&self) -> usize {
+        self.top * self.right * self.bottom * self.left
+    }
+}
 
 fn main() {
     let reader = io::BufReader::new(io::stdin());
@@ -32,6 +58,19 @@ fn main() {
     let n_visible = visibility.iter().filter(|x| **x).count();
 
     println!("{}", n_visible);
+
+    let mut scenic_scores: Array2<Visibility> =
+        Array2::default((forest.shape()[0], forest.shape()[1]));
+
+    populate_scenic_scores(&forest, &mut scenic_scores);
+
+    let max_score = scenic_scores
+        .iter()
+        .map(|v| v.area())
+        .max()
+        .unwrap_or(0usize);
+
+    println!("{}", max_score);
 }
 
 fn set_boundary_visibility(visibility: &mut Array2<bool>) {
@@ -70,5 +109,43 @@ where
             *v = true;
         }
         max_height = max_height.max(*f);
+    }
+}
+
+fn populate_scenic_scores(trees: &Array2<u8>, scenic_scores: &mut Array2<Visibility>) {
+    trees
+        .axis_iter(Axis(0))
+        .zip(scenic_scores.axis_iter_mut(Axis(0)))
+        .for_each(|(t, mut ss)| {
+            populate_scenic_scores_1d(t.iter(), ss.iter_mut().map(|s| &mut s.left));
+            populate_scenic_scores_1d(t.iter().rev(), ss.iter_mut().rev().map(|s| &mut s.right));
+        });
+
+    trees
+        .axis_iter(Axis(1))
+        .zip(scenic_scores.axis_iter_mut(Axis(1)))
+        .for_each(|(t, mut ss)| {
+            populate_scenic_scores_1d(t.iter(), ss.iter_mut().map(|s| &mut s.top));
+            populate_scenic_scores_1d(t.iter().rev(), ss.iter_mut().rev().map(|s| &mut s.bottom));
+        });
+}
+
+fn populate_scenic_scores_1d<'a, T, U>(trees: T, scenic_scores: U)
+where
+    T: DoubleEndedIterator<Item = &'a u8>,
+    U: DoubleEndedIterator<Item = &'a mut usize>,
+{
+    let mut checked_trees = Vec::<u8>::with_capacity(trees.size_hint().1.unwrap_or(0));
+    for (&tree, score) in trees.zip(scenic_scores) {
+        *score = checked_trees
+            .iter()
+            .rev()
+            .take_while(|&&t| t < tree)
+            .count()
+            + 1;
+
+        *score = checked_trees.len().min(*score);
+
+        checked_trees.push(tree);
     }
 }
